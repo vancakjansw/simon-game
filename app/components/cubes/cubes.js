ce68bf39c1b5d973587da61632f122c5bcb6f4ee
Jan Vancak <vancak.jan@gmail.com>
2017-02-23 12:25:06 +0100

    Updated cube component. Code refactor

--- app/components/cubes/cubes.js
+++ app/components/cubes/cubes.js
@@ -27,12 +27,6 @@ class Cubes extends Component {
       greenCube: new Animated.Value(1),
     }
 
-    this.gameData = {
-      gameNumCheck: 0,
-      gameScore: 0,
-      gameCycle: 0,
-    }
-
     this.INFO_STATE = {
       start: 'Start',
       repeat: 'Repeat',
@@ -40,9 +34,9 @@ class Cubes extends Component {
     }
 
     this.GAME_STATE = {
-      start: 'start',
+      playing: 'playing',
+      watch: 'watch',
       stop: 'stop',
-      playing: 'playing'
     }
 
     this.CUBES_ORDER = [
@@ -52,11 +46,18 @@ class Cubes extends Component {
       'yellowCube',
     ]
 
-    this.state = {
+    this.gameData = {
+      gameNumCheck: 0,
+      gameScore: 0,
+      gameCycle: 0,
       gameFields: [],
       gameState: this.GAME_STATE.stop,
-      infoCircle: this.INFO_STATE.start,
     }
+
+    this.state = {
+      gameScore: this.gameData.gameScore,
+      infoCircle: this.INFO_STATE.start,
+     }
   }
 
   animateStartButton() {
@@ -91,35 +92,67 @@ class Cubes extends Component {
     ]).start()
   }
 
-  resetGame() {
-    Alert.alert('Game Over')
-    this.props.setScore(this.gameData.gameScore)
-    this.gameData.gameScore = 0
-    this.gameData.gameNumCheck = 0
-    this.gameData.gameCycle = 0
+  updateInfoCircle() {
+    let infoCircleText = ''
+
+    switch (this.gameData.gameState) {
+      case this.GAME_STATE.playing:
+        infoCircleText = this.INFO_STATE.repeat
+        break
+      case this.GAME_STATE.watch:
+        infoCircleText = this.INFO_STATE.watch
+        break
+      case this.GAME_STATE.stop:
+        infoCircleText = this.INFO_STATE.start
+        break
+    }
 
     this.setState({
-      gameState: this.GAME_STATE.stop,
-      gameFields: [],
-      infoCircle: this.INFO_STATE.start,
+      infoCircle: infoCircleText
     })
   }
 
-  onCubeClick(index: string) {
-    if (this.state.gameState !== this.GAME_STATE.playing) return
+  resetGame() {
+    this.props.setScore(this.gameData.gameScore)
+    this.gameData = {
+      gameNumCheck: 0,
+      gameCycle: 0,
+      gameFields: [],
+      gameState: this.GAME_STATE.stop,
+    }
 
-    let checkArray = this.state.gameFields[this.gameData.gameScore]
+    this.setGameScore(0)
+    this.updateInfoCircle()
+    Alert.alert('Game Over')
+  }
+
+  setGameScore(value): number {
+    if (value === 0) {
+      this.gameData.gameScore = 0
+      this.setState({
+        gameScore: 0
+      })
+    } else {
+      this.gameData.gameScore += 1
+      this.setState({
+        gameScore: this.gameData.gameScore
+      })
+    }
+  }
+
+  onCubeClick(index: string) {
+    if (this.gameData.gameState !== this.GAME_STATE.playing || this.gameData.gameScore > this.gameData.gameFields.length - 1) return
+
+    let checkArray = this.gameData.gameFields[this.gameData.gameScore]
     let numToCheck = checkArray[this.gameData.gameCycle]
 
     this.animateCube(index)
     if (numToCheck === index) {
       this.gameData.gameCycle++
+
       if (this.gameData.gameCycle === checkArray.length) {
-        this.setState({
-          gameCycle: 0,
-        })
         this.gameData.gameCycle = 0
-        this.gameData.gameScore += 1
+        this.setGameScore(1)
         setTimeout(this.runGameCycle, 1200)
       }
     } else {
@@ -128,7 +161,7 @@ class Cubes extends Component {
   }
 
   runGame() {
-    if (this.state.gameState === this.GAME_STATE.stop) {
+    if (this.gameData.gameState === this.GAME_STATE.stop) {
       this.runGameCycle()
     } else {
       this.animateStartButton()
@@ -147,14 +180,12 @@ class Cubes extends Component {
   runGameCycle() {
     this.animateStartButton()
     let cubesToCheck = this.getCubesToCheck()
-    let actualGameFields = this.state.gameFields // workaround
+    let actualGameFields = this.gameData.gameFields // workaround
 
     actualGameFields.push(cubesToCheck)
-    this.setState({
-      gameFields: actualGameFields,
-      gameState: this.GAME_STATE.playing,
-      infoCircle: this.INFO_STATE.watch,
-    })
+    this.gameData.gameState = this.GAME_STATE.watch
+    this.gameData.gameFields = actualGameFields
+    this.updateInfoCircle()
 
     let i = 0
     let nextBlick = () => {
@@ -165,13 +196,12 @@ class Cubes extends Component {
 
       promise.then(() => {
         i++
-        if (i < this.state.gameFields.length) {
+        if (i < this.gameData.gameFields.length) {
           setTimeout(nextBlick, 500)
-        } else {
+        } else if (this.gameData.gameState === this.GAME_STATE.watch) {
           this.animateStartButton()
-          this.setState({
-            infoCircle: this.INFO_STATE.repeat,
-          })
+          this.gameData.gameState = this.GAME_STATE.playing
+          this.updateInfoCircle()
         }
       })
     }
@@ -211,7 +241,7 @@ class Cubes extends Component {
         <View style={styles.controlContainer}>
           <View style={styles.controlContainerInnerLeft}>
             <Text style={styles.controlContainerScore}>
-              Score: {this.gameData.gameScore}
+              Score: {this.state.gameScore}
             </Text>
           </View>
           <View>
